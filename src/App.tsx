3956d45d97acdd42787a8b82004ad25184c815e5
Ariel <ariel.calderon@epn.edu.ec>
2025-05-07 12:23:31 -0500

    Deployment configuration added

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
-import { Route, RouterProvider, Routes } from "react-router-dom";
-import { MainLayout } from "./layouts/MainLayout";
-import { Home } from "./pages/Home";
-import { Shop } from "./pages/Shop";
-import { Cart } from "./pages/Cart";
+import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 
 function App() {
   return (
-
-    // <MainLayout>
-    //   <Routes>
-    //     <Route path="/" element={<Home />} />
-    //     <Route path="/shop" element={<Shop />} />
-    //     <Route path="/cart" element={<Cart />} />
-    //   </Routes>
-    // </MainLayout>
-    <RouterProvider router = {router}/>
+    <RouterProvider router={router} />
   );
 }
 
